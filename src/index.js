1a98abe883e8acbe6840b97bdd9c5cfb37c3006c
fabulouzsage <fabuloussage@yahoo.com>
2023-05-08 18:24:55 +0100

    Antiquity

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import * as dotenv from "dotenv";
+// dotenv.config();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import store from './redux/store';
+import App from './App';
+
+const persistor = persistStore(store);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
